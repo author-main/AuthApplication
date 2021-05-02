798c19f4e51773119d7af02f7538edec7a0d2108
iUser <author-main@mail.ru>
2021-05-02 18:35:45 +0300

    02.05.2021

--- app/src/main/java/example/com/authapplication/AuthModel.kt
+++ app/src/main/java/example/com/authapplication/AuthModel.kt
@@ -1,10 +1,25 @@
 package example.com.authapplication
 
+import android.content.Context
 import android.graphics.Color
 import android.util.Patterns
 import androidx.core.content.ContextCompat
 import example.com.authapplication.AuthApplication.Companion.applicationContext
+import example.com.authapplication.auth_service.AuthFingerPrint
+import example.com.authapplication.auth_service.FirebaseAuthService
+import example.com.authapplication.interfaces.AuthBiometric
+import example.com.authapplication.interfaces.AuthEmailStore
+import example.com.authapplication.interfaces.AuthPasswordStore
+import example.com.authapplication.interfaces.AuthService
+import example.com.authapplication.store.AuthEncryptPasswordStore
+import example.com.authapplication.store.AuthMailStore
 
-class AuthModel {
+class AuthModel(private val context: Context) {
+    private val authService      : AuthService        = FirebaseAuthService()
+    private val emailAddressStore: AuthEmailStore     = AuthMailStore()
+    private val passwordStore    : AuthPasswordStore  = AuthEncryptPasswordStore()
+    private val authBiometric    : AuthBiometric      = AuthFingerPrint(context)
+    init{
 
+    }
 }
